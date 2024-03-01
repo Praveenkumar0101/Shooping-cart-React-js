fd4a96f72fbf9a26fbb2a8378644020a90ee70aa
Praveenkumar0101 <praveenkumar.vancharla@gmail.com>
2024-03-01 11:48:48 +0530

    updated

--- src/products.js
+++ src/products.js
@@ -6,6 +6,16 @@ import product5 from "./assets/products/5.png";
 import product6 from "./assets/products/6.webp";
 import product7 from "./assets/products/7.webp";
 import product8 from "./assets/products/8.webp";
+import product9 from "./assets/products/9.jpg";
+import product10 from "./assets/products/10.jpg";
+import product11 from "./assets/products/11.png";
+import product12 from "./assets/products/12.jpg";
+import product13 from "./assets/products/13.png";
+import product14 from "./assets/products/14.jpg";
+import product15 from "./assets/products/15.jpg";
+import product16 from "./assets/products/16.jpg";
+import product17 from "./assets/products/17.jpg";
+import product18 from "./assets/products/18.webp";
 
 export const PRODUCTS = [
   {
@@ -56,4 +66,65 @@ export const PRODUCTS = [
     price:   1200,
     productImage: product8,
   },
+  {
+    id: 9,
+    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    price:   1200,
+    productImage: product9,
+  },
+  {
+    id: 10,
+    productName: "smart watch",
+    price:   2500,
+    productImage: product10,
+  },
+  {
+    id: 11,
+    productName: "hp i5",
+    price:   6500,
+    productImage: product11,
+  },
+  {
+    id: 12,
+    productName: "Projecter",
+    price:   9500,
+    productImage: product12,
+  },
+  {
+    id: 13,
+    productName: "Canon Printer",
+    price:   6200,
+    productImage: product13,
+  },
+  {
+    id: 14,
+    productName: "intex power bank",
+    price:   1400,
+    productImage: product14,
+  },
+  {
+    id: 15,
+    productName: "Remote car",
+    price:   650,
+    productImage: product15,
+  },
+  {
+    id: 16,
+    productName: "Bedsheets",
+    price:   1600,
+    productImage: product16,
+  },
+  {
+    id: 17,
+    productName: "Kurtis",
+    price:   1300,
+    productImage: product17,
+  },
+  {
+    id: 18,
+    productName: "LG smart tv",
+    price:   20999,
+    productImage: product18,
+  }
+  
 ];
